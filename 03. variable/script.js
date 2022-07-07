68430437bd00cf0de46a862aac3f0e7ca1df52aa
SM Emon Islam <greensmdrem99@gmail.com>
2022-07-07 13:00:46 -0500

    change commit

---
+++ 03. variable/script.js
@@ -0,0 +1,20 @@
+var assign1 = 20;
+    assign1 = 50;
+console.log(assign1);
+
+var assign2 = 30;
+var assign2 = 50;
+console.log(assign2);
+
+let assign3 = 90;
+    assign3 = 100;
+console.log(assign3);
+
+// let assign4 = 20;
+// let assign4 = 50;
+// console.log(assign4)
+
+const assing5 = 20;
+// assing5 = 30;
+
+
