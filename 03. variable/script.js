0ccc4a85da23c8701fba360c63c3f0e844d4b543
SM Emon Islam <greensmdreem99>
2022-07-07 22:26:54 -0500

    var, let, const Concept

--- 03. variable/script.js
+++ 03. variable/script.js
@@ -1,3 +1,7 @@
+/**
+ * Assign and Redeclared
+ */
+
 var assign1 = 20;
     assign1 = 50;
 console.log(assign1);
@@ -14,7 +18,74 @@ console.log(assign3);
 // let assign4 = 50;
 // console.log(assign4)
 
-const assing5 = 20;
+const assign5 = 20;
 // assing5 = 30;
 
 
+/**
+ * Global Scope, Functional Scope, Block Scope
+ */
+
+var scopeVar1 = 10;  // Global Scope
+function varFunc(){ // Functional Scope
+    var scopeVar2 = 20;
+    console.log(scopeVar1)
+    console.log(scopeVar2);
+}
+varFunc();
+
+// console.log(scopeVar2); Not Access
+
+if(true){ // Block Scope
+    var scopeVar3 = 30;
+}
+console.log(scopeVar3);
+
+let scopeLet1 = 15 // Global Scope
+function letFunc(){ // Functional Scope
+    let scopeLet2 = 25;
+    console.log(scopeLet1);
+    console.log(scopeLet2);
+}
+letFunc();
+// console.log(scopeLet2); Not Access
+
+if(true){ // Block Scope
+    let scopeLet3 = 25;
+    console.log(scopeLet3);
+}
+// console.log(scopeLet3); Not Access
+
+const scopeConst1 = 15 // Global Scope
+function constFunc(){
+    const scopeConst2  = 25;
+    console.log(scopeConst1);
+    console.log(scopeConst2);
+}
+constFunc();
+// console.log(scopeLet2); Not Access
+
+if(true){
+    const scopeConst3 = 25;
+    console.log(scopeConst3);
+}
+// console.log(scopeConst3); Not Access
+
+
+let val1 = 10;
+function firstFunc(){
+    let val2 = 20;
+    console.log(val1); // Access
+    console.log(val2); // Access
+    // console.log(val3); // Not Access
+    function secondFunc(){
+        let val3 = 30;
+
+        console.log(val1); // Access
+        console.log(val2); // Access
+        console.log(val3); // Access
+    }
+    secondFunc();
+}
+
+firstFunc();
